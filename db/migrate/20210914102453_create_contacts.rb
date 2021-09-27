a5a01c30401994da43df3b9b8afa0f50a34d0634
yuki010126 <kinyounokyuzitu@gmail.com>
2021-09-27 10:24:43 +0000

    デプロイ

--- db/migrate/20210914102453_create_contacts.rb
+++ db/migrate/20210914102453_create_contacts.rb
@@ -4,7 +4,7 @@ class CreateContacts < ActiveRecord::Migration[5.2]
       t.string :nickname, null: false
       t.string :email, null: false
       t.string :phone_number, null: false
-      t.text :subject, default: 0, null: false
+      t.text :subject, null: false
       t.text :message, null: false
 
 
