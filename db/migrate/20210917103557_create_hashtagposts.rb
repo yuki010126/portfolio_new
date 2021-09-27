04a71fddb9ed8f7df21b1914e94914a0909349e3
yuki010126 <kinyounokyuzitu@gmail.com>
2021-09-27 11:05:58 +0000

    modify migrate hashtagposts

--- db/migrate/20210917103557_create_hashtagposts.rb
+++ db/migrate/20210917103557_create_hashtagposts.rb
@@ -2,7 +2,7 @@ class CreateHashtagposts < ActiveRecord::Migration[5.2]
   def change
     create_table :hashtagposts do |t|
       t.integer :discussion_id
-      t.references :hashtag_id, index: true, foreign_key: true
+      t.references :hashtag, index: true, foreign_key: true
       t.timestamps
     end
   end
