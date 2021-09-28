8ee4b6bd59eca9a8b7f657d0c4a96510797fda7b
yuki010126 <kinyounokyuzitu@gmail.com>
2021-09-28 10:31:50 +0000

    seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create([{name: "会社・職業" }, {name: "社会" }])
