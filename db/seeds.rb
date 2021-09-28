f3f97d9926eceaa684b0e9e317a02fdb631fd5e8
yuki010126 <kinyounokyuzitu@gmail.com>
2021-09-28 11:31:57 +0000

    カテゴリー修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,4 @@
 
 Category.create([{name: "会社・職業" },{name: "地域・文化" },  {name: "スポーツ" }, {name: "IT関係" },{name: "食" }, {name: "起業" }, {name: "災害" }, {name: "世界情勢" }, {name: "学問" }, {name: "政治" }, {name: "その他" }])
 
-kind.create([{name: "会社" },{name: "人間関係" },  {name: "スポーツ" }, {name: "心と身体" },{name: "ネット関係" }, {name: "音楽" },{name: "テレビ" }, {name: "旅行" }, {name: "youtube" },{name: "恋愛" }, {name: "動物" },  {name: "芸能" }, {name: "趣味" }, {name: "その他" }])
+Kind.create([{name: "会社" },{name: "人間関係" },  {name: "スポーツ" }, {name: "心と身体" },{name: "ネット関係" }, {name: "音楽" },{name: "テレビ" }, {name: "旅行" }, {name: "youtube" },{name: "恋愛" }, {name: "動物" },  {name: "芸能" }, {name: "趣味" }, {name: "その他" }])
